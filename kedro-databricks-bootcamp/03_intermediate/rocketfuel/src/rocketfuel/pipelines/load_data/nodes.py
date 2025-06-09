035789fa8a19a410f588d69a9596a71025c6df98
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2025-06-10 01:18:15 +0200

    First draft of Kedro on Databricks
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

---
+++ kedro-databricks-bootcamp/03_intermediate/rocketfuel/src/rocketfuel/pipelines/load_data/nodes.py
@@ -0,0 +1,19 @@
+"""
+This is a boilerplate pipeline 'load_data'
+generated using Kedro 0.19.10
+"""
+
+import pandas as pd
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+
+
+def _noop(df: DataFrame) -> DataFrame:
+    """A no-operation node that simply returns the input Spark DataFrame."""
+    return df
+
+
+def spark_from_pandas(df: pd.DataFrame) -> DataFrame:
+    """Convert a Pandas DataFrame to a Spark DataFrame."""
+    _spark_session = SparkSession.builder.getOrCreate()
+    return _spark_session.createDataFrame(df)
---
+++ kedro-databricks-bootcamp/README.md
@@ -0,0 +1,18 @@
+# Kedro on Databricks Bootcamp
+
+## Overview
+
+1. Day 1
+   - First steps with Kedro
+   - The Kedro Framework
+2. Day 2
+   - Kedro on Databricks
+3. Day 3
+   - Extending Kedro
+   - Kedro in production
+
+## Installation
+
+```
+pip install -r requirements.txt
+```
