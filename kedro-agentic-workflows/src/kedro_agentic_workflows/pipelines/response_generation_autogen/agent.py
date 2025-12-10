b1e28579dfdb4e4c1ccc547a66e0cf8ece2e3897
ElenaKhaustova <ymax70rus@gmail.com>
2025-12-10 13:08:17 +0000

    Implemented response agent
    Signed-off-by: ElenaKhaustova <ymax70rus@gmail.com>

--- kedro-agentic-workflows/src/kedro_agentic_workflows/pipelines/response_generation_autogen/agent.py
+++ kedro-agentic-workflows/src/kedro_agentic_workflows/pipelines/response_generation_autogen/agent.py
@@ -1,8 +1,9 @@
 import asyncio
-import json
+from typing import List
 
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.base import TaskResult
+from langchain_core.messages import AIMessage, BaseMessage
 from pydantic import BaseModel, Field
 
 from ...utils import KedroAgent, AgentContext
@@ -24,6 +25,9 @@ class ResponseGenerationAgentAutogen(KedroAgent):
         self.tool_prompt = self.context.get_prompt("tool_prompt")
         self.llm = self.context.llm
 
+        self.response_prompt = self.context.get_prompt("response_prompt")
+        self.response_agent: AssistantAgent | None = None
+
     def compile(self):
         """Initialize the Agent with its tools and instructions."""
         self.tool_agent = AssistantAgent(
@@ -33,10 +37,20 @@ class ResponseGenerationAgentAutogen(KedroAgent):
             model_client=self.llm,
         )
 
+        self.response_agent = AssistantAgent(
+            name="response_generation_agent_response",
+            system_message="Generate final structured response using tool results.",
+            model_client=self.llm,
+            output_content_type=ResponseOutput
+        )
+
     def invoke(self, context: dict, config: dict | None = None) -> dict:
         """Run the agent and produce the response in the same form as previously."""
         if self.tool_agent is None:
             raise ValueError(f"{self.__class__.__name__} must be compiled before invoking. Call .compile() first.")
+
+        # Collect initial messages
+        messages: List[BaseMessage] = []
 
         dynamic_context = {
             "intent": context["intent"],
@@ -45,13 +59,14 @@ class ResponseGenerationAgentAutogen(KedroAgent):
         }
 
         tool_instructions = self.tool_prompt.format(**dynamic_context)
-
         tool_result: TaskResult = asyncio.run(self.tool_agent.run(task=tool_instructions))
 
         try:
             function_called = tool_result.messages[-2].content[0].name
         except Exception:
             function_called = None
+
+        messages.append(AIMessage(content=tool_result.messages[-1].content))
 
         created_claim = ""
         doc_results = ""
@@ -73,6 +88,14 @@ class ResponseGenerationAgentAutogen(KedroAgent):
             "user_claims": user_claims,
         }
 
-        print(dynamic_context)
+        response_instructions = self.response_prompt.format(**dynamic_context)
+        final_result: TaskResult = asyncio.run(self.response_agent.run(task=response_instructions))
+        response: ResponseOutput = final_result.messages[-1].content
 
-        return {}
+        messages.append(AIMessage(content=response.message))
+
+        return {
+            "messages": messages,
+            "claim_created": response.claim_created,
+            "escalated": response.escalation,
+        }
