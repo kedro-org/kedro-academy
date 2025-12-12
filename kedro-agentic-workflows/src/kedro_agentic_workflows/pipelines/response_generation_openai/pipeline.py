3176f1ed94d839afc2d1fb671729343b247ea57c
ElenaKhaustova <ymax70rus@gmail.com>
2025-12-12 17:04:04 +0000

    Apllied llm node abstraction
    Signed-off-by: ElenaKhaustova <ymax70rus@gmail.com>

--- kedro-agentic-workflows/src/kedro_agentic_workflows/pipelines/response_generation_openai/pipeline.py
+++ kedro-agentic-workflows/src/kedro_agentic_workflows/pipelines/response_generation_openai/pipeline.py
@@ -1,32 +1,25 @@
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, node, pipeline, llm_context_node, tool
 
 from .nodes import (
     generate_response,
-    init_tools,
-    init_response_generation_context,
     log_response_and_end_session,
 )
+from .tools import build_lookup_docs, build_get_user_claims, build_create_claim
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            node(
-                func=init_tools,
-                inputs=["db_engine", "docs", "params:docs_matches"],
-                outputs="tools",
-                name="init_tools_node",
-            ),
-            node(
-                func=init_response_generation_context,
-                inputs=[
-                    "llm_openai",
-                    "tool_prompt",
-                    "response_prompt",
-                    "tools",
-                ],
+            llm_context_node(
+                name="response_agent_context_node",
                 outputs="response_generation_context",
-                name="init_response_generation_context_node",
+                llm="llm_openai",
+                prompts=["tool_prompt", "response_prompt"],
+                tools=[
+                    tool(build_get_user_claims, "db_engine"),
+                    tool(build_lookup_docs, "docs", "params:docs_matches"),
+                    tool(build_create_claim, "db_engine"),
+                ],
             ),
             node(
                 func=generate_response,
