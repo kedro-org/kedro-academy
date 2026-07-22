2f3b4e916033a9f54ce852376d6d526d98fdccf0
Laura Couto <laurarccouto@gmail.com>
2026-07-22 00:57:04 -0300

    Add healthcare GraphRAG project to Kedro-Academy
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

---
+++ healthcare-graphrag/src/graphrag/pipelines/vector_indexing/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'vector_indexing'
+generated using Kedro 1.4.0
+"""
+
+from .pipeline import create_pipeline
+
+__all__ = ["create_pipeline"]
+
+__version__ = "0.1"
